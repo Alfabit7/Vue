370d9c9302cd1578800b18f84fadcea364b5c411
Andrey <tirtg@yandex.ru>
2022-04-25 09:22:58 +0300

    added files homework lesson-2

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to the calculator ;)"/>
-    <input v-model.number="operand1"> <input v-model.number="operand2"> 
-<h1>Результат {{operand1}} {{sign}} {{operand2}} равен {{result}}</h1>
-
-<button @click='summation'>+</button>
-<button @click='substraction' >-</button>
-<button @click='didviding' v-if='(this.operand2!=0)'>\</button>
-<button @click='multiplication'>*</button>
-<button @click='exponentiation'>^</button>
-
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to the calculator ;)" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    HelloWorld,
   },
-
-   data(){
-    return {
-      operand1:'',
-      operand2:'',
-      result: '',
-      sign:''
-     }
-   },
-   methods:{
-      summation(){
-        this.result = this.operand1 + this.operand2;
-        this.sign='+'
-      },
-      substraction(){
-    this.result = this.operand1 - this.operand2;
-    this.sign='-'
-      },
-      didviding(){
-        this.result = this.operand1 / this.operand2;
-        this.sign='/'
-      },
-      multiplication(){
- this.result = this.operand1 * this.operand2;
- this.sign='*'
-      },
-      exponentiation(){
-      this.result = Math.pow(this.operand1, this.operand2)
-      this.sign='^'
-    },
-   }
-
-}
+};
 </script>
 
 <style>
@@ -67,8 +26,9 @@ export default {
   margin-top: 60px;
 }
 
-button{
+button {
   margin: 10px 10px;
-  width: 50px; height: 30px;
+  width: 50px;
+  height: 30px;
 }
 </style>
